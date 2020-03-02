3e4d0b97e261443a8fca1911c36088fdc5337817
Eric Settels, Michelle Morin and Zakkrey Short <michelle@>
2020-03-02 11:01:13 -0800

    add exe output type to csproj file and add UI

--- Leetspeak/Program.cs
+++ Leetspeak/Program.cs
@@ -2,14 +2,23 @@ using System;
 using System.Collections.Generic;
 using Leetspeak.Models;
 
-
 namespace Leetspeak
 {
  public class Program
   {
    public static void Main()
     {
+      string userInput = GetUserInput();
+      LeetspeakTranslator translator = new LeetspeakTranslator();
+      string convertedString = translator.Translate(userInput);
+      Console.WriteLine(convertedString);
+    }
 
+    public static string GetUserInput()
+    {
+      Console.WriteLine("enter a sentence to turn into Leetspeak:");
+      string userInput = Console.ReadLine();
+      return userInput;
     }
   }
 }
