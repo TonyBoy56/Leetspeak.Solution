e5ca206720fd537721a63f0e4f3d4d4b0f9fab86
Antonio Cruz and Michelle Morin <antonio@>
2020-03-09 15:29:08 -0700

    refactor tests based on new constructor argument and removing argument from Translate method

--- Leetspeak.Tests/LeetspeakTranslator.Tests.cs
+++ Leetspeak.Tests/LeetspeakTranslator.Tests.cs
@@ -11,43 +11,49 @@ namespace Leetspeak.Tests
     public void Translate_ConvertsStringInput_String()
     {
       string stringOutput = "hll";
-      LeetspeakTranslator translator = new LeetspeakTranslator();
-      Assert.AreEqual(stringOutput, translator.Translate("hll"));
+      LeetspeakTranslator translator = new LeetspeakTranslator("hll");
+      translator.Translate();
+      Assert.AreEqual(stringOutput, translator.TranslatedInput);
     }
     [TestMethod]
     public void Translate_ConvertsStringInput_StringOutputChangeE()
     {
       string stringOutput = "h3ll";
-      LeetspeakTranslator translator = new LeetspeakTranslator();
-      Assert.AreEqual(stringOutput, translator.Translate("hell"));
+      LeetspeakTranslator translator = new LeetspeakTranslator("hell");
+      translator.Translate();
+      Assert.AreEqual(stringOutput, translator.TranslatedInput);
     }
     [TestMethod]
     public void Translate_ConvertsStringInput_StringOutputChangeO()
     {
       string stringOutput = "h3ll0";
-      LeetspeakTranslator translator = new LeetspeakTranslator();
-      Assert.AreEqual(stringOutput, translator.Translate("hello"));
+      LeetspeakTranslator translator = new LeetspeakTranslator("hello");
+      translator.Translate();
+      Assert.AreEqual(stringOutput, translator.TranslatedInput);
     }
     [TestMethod]
     public void Translate_ConvertsStringInput_StringOutputChangeI()
     {
       string stringOutput = "1 d0";
-      LeetspeakTranslator translator = new LeetspeakTranslator();
-      Assert.AreEqual(stringOutput, translator.Translate("I do"));
+      LeetspeakTranslator translator = new LeetspeakTranslator("I do");
+      translator.Translate();
+      Assert.AreEqual(stringOutput, translator.TranslatedInput);
     }
     [TestMethod]
     public void Translate_ConvertsStringInput_StringOutputChangeT()
     {
       string stringOutput = "L37 g0";
-      LeetspeakTranslator translator = new LeetspeakTranslator();
-      Assert.AreEqual(stringOutput, translator.Translate("Let go"));
+      LeetspeakTranslator translator = new LeetspeakTranslator("Let go");
+      translator.Translate();
+      Assert.AreEqual(stringOutput, translator.TranslatedInput);
     }
     [TestMethod]
     public void Translate_ConvertsStringInput_StringOutputChangeS()
     {
       string stringOutput = "s0 l37z g0";
-      LeetspeakTranslator translator = new LeetspeakTranslator();
-      Assert.AreEqual(stringOutput, translator.Translate("so lets go"));
+      LeetspeakTranslator translator = new LeetspeakTranslator("so lets go");
+      translator.Translate();
+      Assert.AreEqual(stringOutput, translator.TranslatedInput);
     }  
   }
 }
